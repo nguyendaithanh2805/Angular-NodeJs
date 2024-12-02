e2d5f22134f4eeb187886df541245b09ed46489f
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-12-02 17:56:29 +0700

    feat: Create angular guard for authorization when user not permission.

---
+++ client/src/app/guards/auth-role.guard.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { Router } from 'express';
+import { catchError, map, of } from 'rxjs';
+
+export const authRoleGuard: CanActivateFn = (route, state) => {
+  const http = inject(HttpClient);
+  const router = inject(Router);
+
+  return http.get('/api/admin', { observe: 'response' }).pipe(
+    map((response) => {
+      if (response.status === 200)
+        return true;
+      return false;
+    }),
+    catchError((error) => {
+      if (error.status === 403)
+        router.navigate(['/forbidden']);
+      return of(false); // Khong cho phep truy cap
+    })
+  )
+};
