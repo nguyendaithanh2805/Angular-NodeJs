01cc14ba5baac566ef81a251d2096c3219a550e0
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-12-16 15:43:59 +0700

    feat: Create CartService.

---
+++ Server/services/CartService.js
@@ -0,0 +1,40 @@
+const cartRepository = require("../repositories/CartRepository");
+const productRepository = require("../repositories/ProductRepository");
+
+class CartService {
+    async getAllCartAsync() {
+        return await cartRepository.findAllAsync();
+    }
+
+    async getTotalCartAsync() {
+        return await cartRepository.findTotalCartAsync();
+    }
+
+    async getCartByIdAsync(id) {
+        const cart = await cartRepository.findByIdAsync(id);
+        if (cart == null)
+            throw new Error(`Cart with id [${id}] not found.`);
+        return cart;
+    }
+
+    async addCartAsync(cart) {
+        return await cartRepository.addAsync(cart);
+    }
+
+    async updateCartAsync(id, cart) {
+        let existingCart = await this.getCartByIdAsync(id);
+
+        existingCart.userId = cart.userId;
+        existingCart.productId = cart.productId;
+        existingCart.quantity = cart.quantity;
+        existingCart.totalBill = cart.totalBill;
+
+        await cartRepository.updateAsync(id, existingCart);
+    }
+
+    async deleteCartAsync(id) {
+        const cart = await this.getCartByIdAsync(id);
+        await cartRepository.deleteAsync(cart);
+    }
+}
+module.exports = new CartService();
